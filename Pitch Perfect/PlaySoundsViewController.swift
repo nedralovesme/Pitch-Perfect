9110e2265d4377c96f0fd769f875427e42554657
Nedra Mevoli <>
2020-01-22 10:50:17 -0500

    no message

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  playSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Nedra Ferguson on 1/17/20.
+//  Copyright © 2020 Nedra Mevoli. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+
+    @IBAction func playSoundForButton(_ sender: UIButton){
+        print ("play sound button pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject){
+        print ("stop audio button pressed")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(.notPlaying)
+    }
+    
+}
